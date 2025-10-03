675ffdbf9dda801c2bea54546839f2be9391d8a1
devdouglasm <devdouglasm@gmail.com>
2025-10-03 04:50:50 -0300

    metodo de envio de email adicionado

---
+++ src/main/java/com/devdouglasm/notificacao/business/enums/StatusNotificacaoEnum.java
@@ -0,0 +1,7 @@
+package com.devdouglasm.notificacao.business.enums;
+
+public enum StatusNotificacaoEnum {
+
+    PENDENTE, NOTIFICADO, CANCELADO
+
+}
---
+++ src/main/java/com/devdouglasm/notificacao/controller/EmailController.java
@@ -0,0 +1,24 @@
+package com.devdouglasm.notificacao.controller;
+
+import com.devdouglasm.notificacao.business.EmailService;
+import com.devdouglasm.notificacao.business.dto.TarefaDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/email")
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @PostMapping
+    public ResponseEntity<Void> enviaEmail(@RequestBody TarefaDTO dto) {
+        emailService.enviaEmail(dto);
+        return ResponseEntity.ok().build();
+    }
+}
